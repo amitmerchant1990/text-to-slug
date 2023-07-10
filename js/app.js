1061facd7cb5409e36c5d388219015e4f4a7c502
Amit Merchant <bullredeyes@gmail.com>
2023-07-10 19:05:53 +0530

    add ability to copy text to  clipboard.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     $('#texttoslug').on('click', function () {
         textToSlug($('#rawText').val());
-    })
+    });
+
+    $('#copyToClipboard').on('click', function () {
+        copyToClipboard($('#rawText').val());
+    });
 
     const slugify = (str) => str.toLowerCase()
         .replace(/[^a-z0-9]+/g, '-')
@@ -10,5 +14,23 @@ $(document).ready(function () {
     function textToSlug(text) {
         let slug = slugify(text)
         $('#rawText').val(slug)
+    }
+
+    function copyToClipboard(text) {
+        if (text.length === 0)
+            return;
+
+        $('#copyToClipboard').html('Copied to Clipboard');
+
+        let textArea = document.createElement('textarea');
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('Copy');
+        textArea.remove();
+
+        setTimeout(function () {
+            $('#copyToClipboard').html('Copy to Clipboard');
+        }, 2000);
     }
 });
