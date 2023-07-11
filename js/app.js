07d16b1896c055c6135a5968ee45ed988b64ff55
Amit Merchant <bullredeyes@gmail.com>
2023-07-11 10:42:04 +0530

    improve copy to clipboard logic.

--- js/app.js
+++ js/app.js
@@ -5,11 +5,25 @@ const copyToClipboard = document.querySelector("#copyToClipboard");
 input.addEventListener("input", () => (output.value = slugify(input.value)));
 
 copyToClipboard.addEventListener("click", () => {
-    navigator.clipboard.writeText(output.value);
+    if (output.value.length === 0)
+        return;
+
+    copyToClipboard.innerHTML = 'Copied to Clipboard!';
+
+    let textArea = document.createElement('textarea');
+    textArea.value = output.value;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('Copy');
+    textArea.remove();
+
+    setTimeout(function () {
+        copyToClipboard.innerHTML = 'Copy to Clipboard';
+    }, 1000);
 });
 
 const slugify = (str) =>
-  str
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/(^-|-$)+/g, "");
+    str
+        .toLowerCase()
+        .replace(/[^a-z0-9]+/g, "-")
+        .replace(/(^-|-$)+/g, "");
