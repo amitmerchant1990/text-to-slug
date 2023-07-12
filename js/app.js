026e8cb996294165fd182ab2cca7402e2eb7d911
Amit Merchant <bullredeyes@gmail.com>
2023-07-12 10:59:20 +0530

    disable copy to clipboard button until the operation is done.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,30 @@
-const input = document.querySelector("#rawText");
-const output = document.querySelector("#sluggedText");
-const copyToClipboard = document.querySelector("#copyToClipboard");
+const input = document.querySelector('#rawText');
+const output = document.querySelector('#sluggedText');
+const copyToClipboard = document.querySelector('#copyToClipboard');
 
-input.addEventListener("input", () => (output.value = slugify(input.value)));
+input.addEventListener('input', () => (output.value = slugify(input.value)));
 
-copyToClipboard.addEventListener("click", () => {
+copyToClipboard.addEventListener('click', () => {
     if (output.value.length === 0)
         return;
 
     copyToClipboard.innerHTML = 'Copied to Clipboard!';
 
+    copyToClipboard.disabled = true;
+
     navigator.clipboard.writeText(output.value);
 
     setTimeout(function () {
         copyToClipboard.innerHTML = 'Copy to Clipboard';
+        copyToClipboard.disabled = false;
     }, 1000);
 });
 
 const slugify = (text) =>
     replaceAccentedCharacters(text)
         .toLowerCase()
-        .replace(/[^a-z0-9]+/g, "-")
-        .replace(/(^-|-$)+/g, "");
+        .replace(/[^a-z0-9]+/g, '-')
+        .replace(/(^-|-$)+/g, '');
 
 // https://stackoverflow.com/a/70288180/1485183        
 const replaceAccentedCharacters = text =>
